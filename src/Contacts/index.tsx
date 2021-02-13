1be79df584604bf1f52ed5cf317c07746e3fe925
victorbrugnolo <brugnolovictor@gmail.com>
2021-02-13 18:52:29 -0300

    adding pages

---
+++ src/Contacts/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Container, Title} from './styles';
+
+const Contacts: React.FC = () => (
+    <>
+        <Container>
+            <Title>Contacts Page</Title>
+        </Container>
+    </>
+);
+
+export default Contacts;
---
+++ src/Main/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Container, Title} from './styles';
+
+const Main: React.FC = () => (
+    <>
+        <Container>
+            <Title>Main Page</Title>
+        </Container>
+    </>
+);
+
+export default Main;
